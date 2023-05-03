0f734058a0389da1eca7e5510706f629db42b7ad
threewebcode <magestore@outlook.com>
2023-05-03 01:48:19 +0000

    feat:publish packge

--- client-app/src/index.ts
+++ client-app/src/index.ts
@@ -1,4 +1,10 @@
-import { JsonRpcProvider, devnetConnection } from '@mysten/sui.js';
+import { 
+    JsonRpcProvider, 
+    devnetConnection, 
+    Ed25519Keypair, 
+    RawSigner, 
+    TransactionBlock 
+} from "@mysten/sui.js";
 
 const provider = new JsonRpcProvider(devnetConnection);
 
@@ -7,4 +13,42 @@ async function getBlockSequence() {
     console.log(sequence);
 }
 
-getBlockSequence();
+async function makeMoveCall() {
+    const keypair = new Ed25519Keypair();
+    const signer = new RawSigner(keypair, provider);
+    const packageObjectId = '0x...';
+    const tx = new TransactionBlock();
+    tx.moveCall({
+    target: `${packageObjectId}::nft::mint`,
+    arguments: [tx.pure('Example NFT')],
+    });
+    const result = await signer.signAndExecuteTransactionBlock({
+    transactionBlock: tx,
+    });
+    console.log({ result });
+}
+
+
+async function publishPackge() {
+    const { execSync } = require('child_process');
+    // Generate a new Keypair
+    const keypair = new Ed25519Keypair();
+    const provider = new JsonRpcProvider();
+    const signer = new RawSigner(keypair, provider);
+    const { modules, dependencies } = JSON.parse(
+    execSync(
+        `${cliPath} move build --dump-bytecode-as-base64 --path ${packagePath}`,
+        { encoding: 'utf-8' },
+    ),
+    );
+    const tx = new TransactionBlock();
+    const [upgradeCap] = tx.publish({
+    modules,
+    dependencies,
+    });
+    tx.transferObjects([upgradeCap], tx.pure(await signer.getAddress()));
+    const result = await signer.signAndExecuteTransactionBlock({
+    transactionBlock: tx,
+    });
+    console.log({ result });
+}
